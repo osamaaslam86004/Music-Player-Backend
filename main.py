187da91cb8e2ee03ac0a6566b7eb93cc8306bd4f
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-09-06 21:32:21 +0500

    add list of cors headers

--- main.py
+++ main.py
@@ -5,10 +5,24 @@ from database import Base, engine
 
 app = FastAPI()
 
+
+CORS_ALLOWED_ORIGINS = [
+    "https://music-player-ui-ux.vercel.app",
+    "http://127.0.0.1:60640",
+    "https://osama11111.pythonanywhere.com",
+    "https://osamaaslam.pythonanywhere.com",
+    "https://web.postman.co",
+    "https://diverse-intense-whippet.ngrok-free.app",
+    "http://127.0.0.1:5500",
+    "https://resume-builder-integrated-with-resume-api.vercel.app",
+    "https://resume-builder-pwa.vercel.app/",
+]
+
+
 # Add CORS middleware
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://music-player-ui-ux.vercel.app", "http://127.0.0.1:60640"],
+    allow_origins=CORS_ALLOWED_ORIGINS,
     allow_credentials=False,
     allow_methods=["POST", "GET", "OPTIONS"],
     allow_headers=["*"],
