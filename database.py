4bac3704d6150c7985e48f5f2eab0ec43e9cdf34
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-09-07 04:28:13 +0500

    TypeError fixed in database.py added Base = declarative_base()

--- database.py
+++ database.py
@@ -1,4 +1,5 @@
 from sqlalchemy.ext.asyncio import AsyncSession, create_async_engine
+from sqlalchemy.orm import sessionmaker, declarative_base
 from sqlalchemy.orm import sessionmaker
 
 DATABASE_URL = "postgresql+asyncpg://default:A9dGRnxcCk2b@ep-bold-scene-a4j046n4-pooler.us-east-1.aws.neon.tech:5432/verceldb"
@@ -6,10 +7,24 @@ DATABASE_URL = "postgresql+asyncpg://default:A9dGRnxcCk2b@ep-bold-scene-a4j046n4
 # Create the database engine
 engine = create_async_engine(DATABASE_URL, echo=True)
 
+# The declarative_base() function is a synchronous operation that
+# sets up a base class for all of your SQLAlchemy ORM models. This base
+# class holds metadata about your models and is used to map your Python classes to database tables.
+
+
+# Create a base class for your models
+Base = declarative_base()
+
 # Create a configured "Session" class
 AsyncSessionLocal = sessionmaker(
     bind=engine, class_=AsyncSession, expire_on_commit=False
 )
+
+
+# Asynchronous operations in SQLAlchemy typically involve operations
+# with the database engine, session, and queries, which are handled
+# using the asyncpg driver and AsyncSession for non-blocking,
+# asynchronous interactions with the database.
 
 
 # Dependency to get the database session in FastAPI routes
