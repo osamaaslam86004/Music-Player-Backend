17ed749d3540c5ce0b5c4480c7daac28fd344252
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-09-08 08:47:10 +0500

    trying to check if vercel deploy this commit

--- play_music_track/models/models.py
+++ play_music_track/models/models.py
@@ -9,3 +9,12 @@ class Audio(Base):
     url = Column(String, nullable=False)
     track_name = Column(String, nullable=False)
     author_name = Column(String, nullable=False)
+
+
+# class Audio(Base):
+#     __tablename__ = "audios"
+
+#     id = Column(Integer, primary_key=True, index=True)
+#     url = Column(String, nullable=False)
+#     track_name = Column(String, nullable=False)
+#     author_name = Column(String, nullable=False)
