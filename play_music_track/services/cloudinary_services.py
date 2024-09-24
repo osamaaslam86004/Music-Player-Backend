079efef4271cbc591b631df3f20da174d880be69
osamaaslam86004 <osama.aslam.86004@gmail.com>
2024-09-24 19:46:32 +0500

    cloudinary_services.py add await add cloudinary.config

--- play_music_track/services/cloudinary_services.py
+++ play_music_track/services/cloudinary_services.py
@@ -1,12 +1,26 @@
 from fastapi.responses import JSONResponse
 from fastapi import status
 from play_music_track.config import cloudinary
+import cloudinary
+
+# Cloudinary
+CLOUDINARY_CLOUD_NAME = "dh8vfw5u0"
+CLOUDINARY_API_KEY = "667912285456865"
+CLOUDINARY_API_SECRET = "QaF0OnEY-W1v2GufFKdOjo3KQm8"
+cloudinary_url = "cloudinary://667912285456865:QaF0OnEY-W1v2GufFKdOjo3KQm8@dh8vfw5u0"
+
+
+cloudinary.config(
+    cloud_name=CLOUDINARY_CLOUD_NAME,
+    api_key=CLOUDINARY_API_KEY,
+    api_secret=CLOUDINARY_API_SECRET,
+)
 from cloudinary.uploader import upload
 
 
 async def upload_to_cloudinary(file):
     try:
-        cloudinary_response = cloudinary.uploader.upload(
+        cloudinary_response = await upload(
             file,
             resource_type="auto",
         )
@@ -15,6 +29,6 @@ async def upload_to_cloudinary(file):
 
     except Exception as e:
         return JSONResponse(
-            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR,
+            status_code=status.HTTP_503_SERVICE_UNAVAILABLE,
             content=f"Cloudinary error: {str(e)}",
         )
